9e1c713a9fbdc5a2d625ff119b9687b9f97b9789
Vaishnavii-23 <vghuge1323@gmail.com>
2025-12-16 22:47:04 +0530

    Adjust Vite configuration for local and production

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,29 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig(({ mode }) => ({
+  // IMPORTANT for GitHub Pages deployment
   base: mode === "production" ? "/portfolio/" : "/",
+
   plugins: [react()],
+
   resolve: {
     extensions: [".js", ".jsx", ".ts", ".tsx", ".json"],
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
   build: {
     outDir: "dist",
+    emptyOutDir: true,
   },
+
   server: {
     port: 3000,
+    open: true,
+  },
+
+  preview: {
+    port: 4173,
   },
 }));
